b6945310901719a90d96e9c97e0481da3656acce
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-04-02 15:52:40 -0400

    Implement for POSIX and C89
    All but the interactive prompts are now implemented for POSIX and C89
    systems.  WIN32 specifics are still only stubs.

--- mv.c
+++ mv.c
@@ -1,22 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h> /* for getopt TODO: freegetopt */
 
 #define INTERACTIVE_ASSUME_YES 1
 #define INTERACTIVE_ON_ERROR   2 /* POSIX says if "the standard input is a terminal"... no way to check that portably */
 #define INTERACTIVE            3
 
-#define RENAME_ON_REBOOT 10
+#define RENAME_ON_REBOOT (EXIT_FAILURE+1)
 
 char *program_name;
 
+#if defined(_WIN32)
+	/* TODO */
+#elif defined(__unix__)
+	#include <libgen.h>
+	#include <sys/stat.h>
+	int isdir(const char *path) {
+		struct stat st;
+		return (!stat(path, &st) && S_ISDIR(st.st_mode));
+	}
+	char *build_dst(const char *src, const char *dst) {
+		if(isdir(dst)) {
+			char *path = strdup(dst);
+			char *file = basename((char*)src);
+			if(!path || !file) return NULL;
+			path = realloc(path, strlen(path) + strlen(file) + 2);
+			if(!path) return NULL;
+			if(path[strlen(path)-1] != '/') {
+				strcat(path, "/");
+			}
+			strcat(path, file);
+			return path;
+		} else {
+			return strdup(dst);
+		}
+	}
+	int file_exists(const char *dst) {
+		FILE *fp;
+		int exists = 0;
+		fp = fopen(dst, "rb");
+		if(fp) { /* The file exists */
+			exists = 1;
+			fclose(fp);
+		}
+		return exists;
+	}
+#endif
+
 int do_move(const char *src, const char *dst, int interactive) {
-	/* if(interactive > INTERACTIVE_ASSUME_YES && dst exists) prompt for overwrite, return 0 on no */
+	char *built_dst = NULL;
+	int err = 0;
+	/* If renning in interactive mode, prompt for overwrite. */
+	if(interactive > INTERACTIVE_ASSUME_YES) {
+	#if defined(_WIN32) || defined(__unix__)
+		built_dst = build_dst(src, dst);
+		if(!built_dst) {
+			perror("do_move call to build_dst failed");
+			exit(EXIT_FAILURE);
+		}
+		if(file_exists(built_dst)) {
+			/* TODO: Prompt for overwrite */
+		}
+	#else
+		/* C89 gives us no way to construct the path, so just always prompt. */
+		/* TODO: Prompt for move */
+	#endif
+	}
 	/* Try to move */
-	/* if(interactive > INERACTIVE_ASSUME_YES && dst is in use) prompt for rename-at-reboot return EXIT_FAILURE on no, RENAME_ON_REBOOT on yes. XXX: Not in POSIX */
-	/* if interactive == INTERACTIVE_ASSUME_YES && dst is in use) rename-at reboot. return RENAME_ON_REBOOT. XXX: Not in POSIX */
-	/* if(any other error) print errmsg. return EXIT_FAILURE */
-	return 0;
+	#if defined(_WIN32)
+		/* if(interactive > INERACTIVE_ASSUME_YES && dst is in use) prompt for rename-at-reboot return EXIT_FAILURE on no, RENAME_ON_REBOOT on yes. XXX: Not in POSIX */
+		/* if interactive == INTERACTIVE_ASSUME_YES && dst is in use) rename-at reboot. return RENAME_ON_REBOOT. XXX: Not in POSIX */
+		/* TODO */
+	#else
+		if(built_dst) {
+			err = rename(src, built_dst);
+		} else {
+			err = rename(src, dst);
+		}
+		if(err) {
+			err = errno;
+		}
+	#endif
+	if(built_dst) {
+		free(built_dst);
+	}
+	return err;
 }
 
 void help(void) {
@@ -28,7 +98,6 @@ int main(int argc, char *argv[]) {
 	int i, err;
 	int exit_status = EXIT_SUCCESS;
 	int interactive = INTERACTIVE_ON_ERROR;
-Is random.
 	program_name = argv[0];
 
 	while((i = getopt(argc, argv, "if")) != -1) {
@@ -52,7 +121,7 @@ Is random.
 	for(i = 0; i < argc-1; i++) {
 		err = do_move(argv[i], argv[argc-1], interactive);
 		if(err) {
-			fprintf(stderr, "%s: move of '%s' to '%s' failed.", program_name, argv[i], argv[argc-1]);
+			fprintf(stderr, "%s: move of '%s' to '%s' failed.\n", program_name, argv[i], argv[argc-1]);
 			exit_status |= err;
 		}
 	}
